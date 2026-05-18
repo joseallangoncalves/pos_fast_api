b91446b561d313ec0eada99f6680cc41733604e9
JOSE ALLAN <joseallan@ufg.br>
2026-05-18 12:11:06 -0300

    Versionamento e apitoken

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from fastapi import status
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel, Field
@@ -18,6 +19,19 @@ app = FastAPI(
     },
 )
 
+API_TOKEN = "123"
+
+
+# Passando o número 1 e 2 no corpo da requisição
+class Numeros(BaseModel):
+    numero1: int = Field(5, description="Primeiro número")
+    numero2: int = Field(3, description="Segundo número")
+    api_token: str = Field(..., description="Token de autenticação")
+
+# Passando o número 1 e 2 no corpo da requisição
+class Resultado(BaseModel):
+    resultado: int
+
 @app.get("/")
 def read_root():
     return {"message": "Servidor FastAPI rodando com sucesso!"}
@@ -27,36 +41,52 @@ def get_status():
     return {"status": "online"}
 
 # Passando o número 1 e 2 no corpo da requisição
-@app.get(path="/soma/{numero1}/{numero2}", tags=["Operações matemáticas"])
-def soma(numero1: int, numero2: int):
+@app.get(
+     path="/soma/v1/{numero1}/{numero2}",
+     summary="Soma dois números inteiros",
+     description="Recebe dois números inteiros e retorna a soma",
+     tags=["Operações matemáticas"]
+    )
+def soma(numero1: int, numero2: int, api_token: str):
+    if api_token != API_TOKEN:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token de autenticação inválido")
+
     total = numero1 + numero2
+    if total < 0 :
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Resultado negativo")
     return {"resultado": total}
 
 # Passando o número 1 e 2 no corpo da requisição
-@app.post("/soma_formato2", tags=["Operações matemáticas"])
-def soma_formato2(numero1: int, numero2: int):
+@app.post(
+    path="/soma/v2",
+    response_model=Resultado,
+    status_code=status.HTTP_200_OK,
+    tags=["Operações matemáticas"],
+    summary="Será descontinuado em 15/06/2026",
+    deprecated=True
+)
+def soma_formato2(numero1: int, numero2: int, api_token: str):
+    if api_token != API_TOKEN:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token de autenticação inválido")
+
     total = numero1 + numero2
     return {"resultado": total}
 
 
-# Passando o número 1 e 2 no corpo da requisição
-class Numeros(BaseModel):
-    numero1: int = Field(..., gt=0)
-    numero2: int = Field(..., gt=0)
-
-# Passando o número 1 e 2 no corpo da requisição
-class Resultado(BaseModel):
-    resultado: int
-
-@app.post("/soma_formato3", 
-          response_model=Resultado,
-          summary="Soma dois números",
-          description="Soma dois números inteiros e retorna o resultado",
-          tags=["Operações matemáticas"],
-        status_code=status.HTTP_200_OK,
-        response_description="Prcessamento feito com sucesso!",
-        )
-        
+@app.post(
+     path="/soma/v3",
+     response_model=Resultado,
+     summary="Soma dois números",
+     description="Soma dois números inteiros e retorna o resultado",
+     tags=["Operações matemáticas"],
+     status_code=status.HTTP_200_OK,
+     response_description="Prcessamento feito com sucesso!"
+     )
 def soma_formato3(numeros: Numeros):
+    if numeros.api_token != API_TOKEN:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token de autenticação inválido")
+    if numeros.numero1 < 0 or numeros.numero2 < 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Números devem ser positivos")
+    
     total = numeros.numero1 + numeros.numero2
     return {"resultado": total}
